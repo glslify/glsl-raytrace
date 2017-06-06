9be7b422a5744e0ac30f9b7ac593f788ace999f1
fand <fand.gmork@gmail.com>
2017-06-06 22:32:56 +0900

    Add reduction to optional parameter

--- index.glsl
+++ index.glsl
@@ -1,7 +1,7 @@
 // Originally sourced from https://www.shadertoy.com/view/ldfSWs
 // Thank you Iñigo :)
 
-vec2 calcRayIntersection(vec3 rayOrigin, vec3 rayDir, float maxd, float precis) {
+vec2 calcRayIntersection(vec3 rayOrigin, vec3 rayDir, float maxd, float precis, float reduction) {
   float latest = precis * 2.0;
   float dist   = +0.0;
   float type   = -1.0;
@@ -14,7 +14,7 @@ vec2 calcRayIntersection(vec3 rayOrigin, vec3 rayDir, float maxd, float precis) 
 
     latest = result.x;
     type   = result.y;
-    dist  += latest;
+    dist  += latest * reduction;
   }
 
   if (dist < maxd) {
@@ -25,7 +25,7 @@ vec2 calcRayIntersection(vec3 rayOrigin, vec3 rayDir, float maxd, float precis) 
 }
 
 vec2 calcRayIntersection(vec3 rayOrigin, vec3 rayDir) {
-  return calcRayIntersection(rayOrigin, rayDir, 20.0, 0.001);
+  return calcRayIntersection(rayOrigin, rayDir, 20.0, 0.001, 1.0);
 }
 
 #pragma glslify: export(calcRayIntersection)
